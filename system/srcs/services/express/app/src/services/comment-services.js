a0c2b148593277318393ce4b7a0af78f4f1c63fe
derakoto <derakoto@student.42antananarivo.mg>
2026-01-13 22:38:11 -0500

    Fix : put the UserId value when saving a comment, limit column to give to return when getting comments of a post

--- system/srcs/services/express/app/src/services/comment-services.js
+++ system/srcs/services/express/app/src/services/comment-services.js
@@ -17,7 +17,8 @@ const newPost = async (req) => {
     }
     await Comment.create({
         content: postContent,
-        PostId: postId
+        PostId: postId,
+        UserId: user
     });
 }
 
@@ -25,7 +26,8 @@ const getPostComments = async (req) => {
     const comments = await Comment.findAll({
         where: {
             PostId : req.params.post_id
-        }
+        },
+        attributes: ["id", "content" ,"createdAt", "UserId", "PostId"]
     })
 
     return (comments);
