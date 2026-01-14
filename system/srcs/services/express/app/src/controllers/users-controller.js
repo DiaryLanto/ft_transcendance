363fbd066d57df726d5ad193abd7a668dee6a0ca
Desire Rakotoarilantonirina <derakoto@student.42antananarivo.mg>
2026-01-14 20:20:45 +0300

    Fix (Login) : check wether login and password is given before calling the service

--- system/srcs/services/express/app/src/controllers/users-controller.js
+++ system/srcs/services/express/app/src/controllers/users-controller.js
@@ -1,17 +1,23 @@
 const user = require('../models/users');
 const {signupUser, loginUser} = require('../services/user-services');
+const AppError = require("../errors/appError");
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     try{
         await signupUser(req.body);
-        res.status(200).json({info: 'User signed up successfully'});
+        res.status(200).json({message: 'User signed up successfully'});
     }
     catch (error){
         next(error);
     }
 }
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
+    if (!req.body.login || !req.body.passwd)
+    {
+        const error = new AppError(400, "Bad request");
+        next (error);
+    }
     try {
         const {user, token} = await loginUser(req.body);
         res.status(201).json({
