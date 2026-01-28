154335785b0665d02b3da668dde5cff6b521d6da
Desire Rakotoarilantonirina <derakoto@student.42antananarivo.mg>
2026-01-28 22:13:19 +0300

    fix (post-controller): remove postId presence check, use data instead of error for dat from db, use post deleted instead of just deleted

--- system/srcs/services/express/app/src/controllers/posts-controller.js
+++ system/srcs/services/express/app/src/controllers/posts-controller.js
@@ -26,13 +26,11 @@ const fetchBlogPosts = async (req, res, next) => {
 const getSpecificPost = async (req, res, next) => {
     try {
         const postId = req.params.post_id;
-        if (!postId)
-            return res.status(404).json({error: "forbidden"});
         const post = await getPost(postId);
         if (!post)
             res.status(404).json({error: "post no found"});
         else
-            res.status(200).json({error: post});
+            res.status(200).json({data: post});
     } catch (error) {
         next(error);
     }
@@ -43,7 +41,7 @@ const deletePost = async (req, res, next) =>{
         return res.status(404).json({error: "forbidden"});
     try {
         await deletePostService(req.params.post_id, req.user.sub);
-        res.status(200).json({message: "deleted"});
+        res.status(200).json({message: "post deleted"});
     } catch (error) {
         next(error);
     }
