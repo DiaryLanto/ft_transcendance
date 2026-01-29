84457dac4df76d0d2b2c0bf1fbe9bb903bd64b6d
Desire Rakotoarilantonirina <derakoto@student.42antananarivo.mg>
2026-01-29 05:50:32 +0300

    Fix : remove the unused query parameter on the endpoint for getting comments by user

--- system/srcs/services/express/app/src/routes/comment-routes.js
+++ system/srcs/services/express/app/src/routes/comment-routes.js
@@ -5,7 +5,7 @@ const error_checker = require('../middlewares/error-checking-validator');
 const {createPost, getCommentOfPost, handleCommentUpdate, handleCommentDeletion, handleGetCommentByUser, handleApproveComment} = require('../controllers/comments-controller');
 
 comment_router.get("/of/:post_id", commentParamValidator, error_checker, getCommentOfPost);
-comment_router.get("/by/:userId", auth_validation, handleGetCommentByUser);
+comment_router.get("/by", auth_validation, handleGetCommentByUser);
 comment_router.post("/", auth_validation, commentValidator, error_checker, createPost);
 comment_router.patch("/:commentId/approve", auth_validation, handleApproveComment);
 comment_router.patch("/:commentId", auth_validation, validateCommentUpdate, error_checker, handleCommentUpdate);
